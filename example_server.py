8cbc8bec076ad660edeedf949928b5303742db3d
Phil Crump <phil@philcrump.co.uk>
2016-03-07 19:35:40 +0000

    WIP TCP server.

--- example_server.py
+++ example_server.py
@@ -11,11 +11,28 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
+# Connection variables
+az = 0
+el = 0
+
 conn, addr = s.accept()
-print 'Connection address:', addr
+print('Received connection from:', addr)
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
+    print("received data:", data)
+    # Parse string
+    pdata = data.split(",")
+    try:
+        az = float(pdata[0])
+    except ValueError:
+        print("Error parsing Azimuth: '",pdata[0],"'")
+        break
+    try:
+        el = float(pdata[1])
+    except ValueError:
+        print("Error parsing Elevation: '",pdata[1],"'")
+        break
+    print("AZ: ",az)
+    print("EL: ",el)
 conn.close()
